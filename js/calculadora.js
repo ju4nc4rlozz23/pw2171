a964f5b13b8f73e997099bfdd6d9f84a179dcb62
ju4nc4rlozz23 <ju4nc4rloz@hotmail.com>
2017-04-04 17:26:46 -0600

    Calculadora2

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,3 +1,55 @@
+//Varible GLOBAL :D
+var operador="";
+
+function borrar(){
+	operador = "";
+	document.calculadora.op1.value = 0;
+	document.calculadora.op2.value = 0;
+	document.calculadora.res.value = 0;
+}
+
+function igual(){
+	var valor1 = document.calculadora.op1.value;
+	var valor2 = document.calculadora.op2.value;
+
+	document.calculadora.res.value = eval(valor1+operador+valor2);
+
+}
+
+function operadores(ope){
+	operador = ope
+}
+
+function numeros(num){
+	if(operador == ""){ //Escribir en el operando1 
+		var valorInicial = document.calculadora.op1.value
+
+		if(valorInicial=="0"){
+			//Elimino el cero
+			document.calculadora.op1.value = "";
+		}
+
+		//Concatena los valores de num con los del operando1
+		document.calculadora.op1.value =
+		document.calculadora.op1.value + num
+	}else{ //Escribo en el operando2
+		var valorInicial = document.calculadora.op2.value
+
+		if(valorInicial=="0"){
+			//Elimino el cero
+			document.calculadora.op2.value = "";
+		}
+
+		//Concatena los valores de num con los del operando1
+		document.calculadora.op2.value =
+		document.calculadora.op2.value + num
+	}
+}
+
+
+//juan carlos
+// TAREA!! SOLO UN CUADRO DE TEXTO
+/*
 //JavaScript de la Calculadora
 
 //variable global
@@ -52,3 +104,5 @@ function numeros(num){
 
 TAREA 
 En un solo cuadro de texto
+
+*/
