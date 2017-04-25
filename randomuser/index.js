f264fbb4a0f23dcd1f2213486806c8c6833858d3
ju4nc4rlozz23 <ju4nc4rloz@hotmail.com>
2017-04-25 15:00:16 -0600

    randomuser con fotos

--- randomuser/index.js
+++ randomuser/index.js
@@ -1,2 +1,19 @@
-const rq = require('electron-require');
+const rq   = require('electron-require');
 const main = rq.remote('./main.js');
+const $    = require("jquery");
+
+function datosRandom(){
+	$.ajax({
+	  url: 'https://randomuser.me/api/',
+	  dataType: 'json',
+	  success: function(data) {
+	    //console.log(data);
+	  	$("#txtNombre").html(data.results[0].name.first+" "+data.results[0].name.last)  //si fuera tipo input se llenaria con .val
+	  	$("#imgFoto").attr("src",data.results[0].picture.large)
+	  },
+	  error(a,b,c){
+	  	alert("Sin internet o sin servidor");
+	  }
+	});
+}
+$("#btnInfo").on("click",datosRandom);
